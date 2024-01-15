098a8369abc73a1ac91cef18bed28ac116ce98ae
Najash Marron <najashmarron@gmail.com>
2024-01-15 13:57:02 -0800

    Adding Cars descriptions

--- src/lib/ExperienceInfo.ts
+++ src/lib/ExperienceInfo.ts
@@ -3,7 +3,7 @@ import { ExperienceDataProps } from "../types/All";
 export const ExperienceData: ExperienceDataProps[] = [
     {
         id: 1,
-        company: "PayPal1",
+        company: "PayPal",
         years: "7",
         image: '/images/paypal.png',
         link: { value: 'https://www.paypal.com', name: "PayPal.com"},
@@ -12,30 +12,38 @@ export const ExperienceData: ExperienceDataProps[] = [
     },
     {
         id: 2,
-        company: "PayPal4",
+        company: "Sibatel",
         years: "7",
-        stack: "JS",
-        image: '/images/sibatel.png'
+        image: '/images/sibatel.png',
+        link: { value: 'https://www.sibatel.com', name: "Sibatel.com"},
+        description: "Worked as a Full Stack Developer enhancing PayPal's Expeirience on Checkout Team, Merchant Team and Braintree.",
+        stack: "React, Redux, Node, Express, GraphQL, Java, Ruby",
     },
     {
         id: 3,
-        company: "PayPal5",
+        company: "Gameloft",
         years: "7",
-        stack: "JS",
-        image: '/images/gameloft.png'
+        image: '/images/gameloft.png',
+        link: { value: 'https://www.gameloft.com', name: "Gameloft.com"},
+        description: "Worked as a Full Stack Developer enhancing PayPal's Expeirience on Checkout Team, Merchant Team and Braintree.",
+        stack: "React, Redux, Node, Express, GraphQL, Java, Ruby",
     },
     {
         id: 4,
-        company: "PayPal2",
+        company: "Code Services",
         years: "7",
-        stack: "JS",
-        image: '/images/codeservices.png'
+        image: '/images/codeservices.png',
+        link: { value: 'https://www.codeservices.com', name: "codeservices.com"},
+        description: "Worked as a Full Stack Developer enhancing PayPal's Expeirience on Checkout Team, Merchant Team and Braintree.",
+        stack: "React, Redux, Node, Express, GraphQL, Java, Ruby",
     },
     {
         id: 5,
-        company: "PayPal3",
+        company: "Lowrance",
         years: "7",
-        stack: "JS",
-        image: '/images/lowrance.svg'
+        image: '/images/lowrance.svg',
+        link: { value: 'https://www.lowrance.com', name: "lowrance.com"},
+        description: "Worked as a Full Stack Developer enhancing PayPal's Expeirience on Checkout Team, Merchant Team and Braintree.",
+        stack: "React, Redux, Node, Express, GraphQL, Java, Ruby",
     },
 ];
