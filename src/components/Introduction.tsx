483651f90d08b33f83d9dd3f3f3a3dda7d0a484c
Najash Marron <najashmarron@gmail.com>
2024-01-16 14:13:51 -0800

    Enhancing <a> tags with aria-labels for screen readers

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -18,17 +18,19 @@ const Introduction = () => {
                     </ul>
                     <div className="social-networks">
                         <div className="social-networks-wrapper">
-                            <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/najashmarron/'><i className="fab fa-linkedin fa-2x"></i></a>
+                            <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/najashmarron/' aria-label="View Najash Marron on LinkedIn">
+                                <i className="fab fa-linkedin fa-2x"></i>
+                            </a>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a><i className="fab fa-stack-overflow fa-2x"></i></a>
+                            <a aria-label="View Najash Marron on LinkedIn"><i className="fab fa-stack-overflow fa-2x"></i></a>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a><i className="fab fa-facebook fa-2x"></i></a>
+                            <a aria-label="View Najash Marron on Facebook"><i className="fab fa-facebook fa-2x"></i></a>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a><i className="fab fa-youtube fa-2x"></i></a>
+                            <a aria-label="View Najash Marron on Youtube"><i className="fab fa-youtube fa-2x"></i></a>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a><i className="fab fa-twitter fa-2x"></i></a>
+                            <a aria-label="View Najash Marron on Twitter"><i className="fab fa-twitter fa-2x"></i></a>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                            <a><i className="fab fa-instagram fa-2x"></i></a>
+                            <a aria-label="View Najash Marron on Instagram"><i className="fab fa-instagram fa-2x"></i></a>
                         </div>
                     </div>
                 </div>
